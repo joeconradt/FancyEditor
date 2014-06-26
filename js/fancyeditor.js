ce5e58b797e41c8dd44a56378a063c8d8b90b050
Joe Conradt <joeconradt@wi.rr.com>
2014-06-25 21:18:47 -0500

    Box templates and forms (plugin based), ruler, tabs

--- js/fancyeditor.js
+++ js/fancyeditor.js
@@ -53,6 +53,8 @@ $(document).ready(function() {
 		}
 	});
 
+	
+
 	$("#new-box-form").submit(function(e) {
 		e.preventDefault();
 
@@ -60,22 +62,16 @@ $(document).ready(function() {
 		var type  = $(this).find("select[name=type]").val();
 		
 
-		// load template
-		$.get("box-types/" + type + ".html", function(data) {
-			templates[type] = data;
-			data = data.replace("{{type}}", type);
-			data = data.replace("{{width}}", width);
-			data = data.replace("{{content}}", "");
+		
+	});
 
-			console.log(data);
+	// tabs
+	$(".js-tab .tab").click(function() {
+		$(".js-tab .tab").addClass("secondary");
+		$(this).removeClass("secondary");
 
-			$("#page").append(data);
-
-			// add event to new fancy box
-			$(".fancy-box").click(function() {
-				changeActiveBox($(this));
-			});
-		});
+		$(".js-tab .tab-content").hide();
+		$($(this).attr("data-target")).show();
 	});
 });
 
@@ -86,12 +82,56 @@ function pageZoom(_zoom) {
 	zoom += _zoom;
 	$("#page").css('zoom', (zoom * 100) + "%");
 }
-function changeActiveBox(box)
-{
+function changeActiveBox(box) {
 	// remove active from any fancy box 
 	$(".fancy-box").removeClass("active");
 	box.addClass("active");
 	activeFancyBox = $(this);
 	$(".fancy-box .options").remove();
 	box.append("<div class='options'></options>");
+}
+function createBoxFormModal(type) {
+	$.get("type/" + type + "/form.html", function(data) {
+		console.log(data);
+		$(".js-modal-form").html(data);
+		formListen();
+	});
+	$("#new-box-modal").foundation("reveal", "open");
+
+}
+function formListen()
+{
+	$("form.option-form").submit(function(e) {
+		e.preventDefault();
+
+		var options = {};
+		$(this).find(".option-control").each(function() {
+			var value = $(this).val();
+			var key = $(this).attr("name");
+			options[key] = value;
+		});
+
+		if(options['type'] == undefined) {
+			alert("Error: box type missing from form.");
+		} else {
+
+			// load template
+			$.get("type/" + options['type'] + "/template.html", function(data) {
+				templates[options['type']] = data;
+
+				for(var property in options) {
+					data = data.replace("{{" + property + "}}", options[property]);
+				}
+
+				$("#page").append(data);
+
+				// add event to new fancy box
+				$(".fancy-box").click(function() {
+					changeActiveBox($(this));
+				});
+			});
+
+		}
+
+	});
 }
